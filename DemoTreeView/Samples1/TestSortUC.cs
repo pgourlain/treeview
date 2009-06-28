4b254a0812ce934701503da27b3584bd93ecfdb7
pgourlain_cp <SND\pgourlain_cp>
2009-06-28 17:20:10 +0000

    correction exemples

---
+++ DemoTreeView/Samples1/TestSortUC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DemoTreeView.Samples1
+{
+    public partial class TestSortUC : UserControl
+    {
+        public TestSortUC()
+        {
+            InitializeComponent();
+        }
+
+        private void TestSortUC_Load(object sender, EventArgs e)
+        {
+            //gtv.Add(
+        }
+    }
+}
